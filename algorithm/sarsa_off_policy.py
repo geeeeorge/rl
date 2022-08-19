5043695e198c48bc64b20fea33c5aba45301378d
Geeeeorge <yuto.george@icloud.com>
2022-08-19 15:03:18 +0900

    方策オフ型sarsa

--- algorithm/sarsa_off_policy.py
+++ algorithm/sarsa_off_policy.py
@@ -5,7 +5,7 @@ from env.gridworld import GridWorld
 from util.greedy_probs import greedy_probs
 
 
-class SarsaAgent:
+class SarsaOffPolicyAgent:
     def __init__(self):
         self.gamma = 0.9
         self.alpha = 0.8
@@ -13,12 +13,13 @@ class SarsaAgent:
         self.action_size = 4
 
         random_actions = {0: 0.25, 1: 0.25, 2: 0.25, 3: 0.25}
-        self.pi = defaultdict(lambda: random_actions)
+        self.pi = defaultdict(lambda: random_actions)  #  ターゲット方策
+        self.b = defaultdict(lambda: random_actions)  # 挙動方策
         self.Q = defaultdict(lambda: 0)
         self.memory = deque(maxlen=2)  # SARSAを保持
 
     def get_action(self, state):
-        action_probs = self.pi[state]
+        action_probs = self.b[state]  # 挙動方策からサンプリング
         actions = list(action_probs.keys())
         probs = list(action_probs.values())
         return np.random.choice(actions, p=probs)
@@ -34,18 +35,27 @@ class SarsaAgent:
         state, action, reward, done = self.memory[0]
         next_state, next_action, _, _ = self.memory[1]
 
-        next_q = 0 if done else self.Q[next_state, next_action]
-        # TDターゲットはR_t + γ * q(S_t+1, A_t+1)
-        target = reward + self.gamma * next_q
+        if done:
+            next_q = 0
+            rho = 1
+        else:
+            next_q = self.Q[next_state, next_action]
+            # 方策オフ型 -> 方策重点サンプリング
+            rho = self.pi[next_state][next_action] / self.b[next_state][next_action]
+
+        target = rho * (reward + self.gamma * next_q)
         self.Q[state, action] += (target - self.Q[state, action]) * self.alpha
 
         # 方策改善
-        self.pi[state] = greedy_probs(self.Q, state, self.epsilon)
+        # ターゲット方策はgreedy
+        self.pi[state] = greedy_probs(self.Q, state, 0)
+        # 挙動方策はε-greedy
+        self.b[state] = greedy_probs(self.Q, state, self.epsilon)
 
 
 if __name__ == '__main__':
     env = GridWorld()
-    agent = SarsaAgent()
+    agent = SarsaOffPolicyAgent()
 
     episodes = 10000
     for episode in range(episodes):
